9b6ea470c905f07f3bfd98572135cf093da14ffa
Jim Barrett <jimbarrett27@gmail.com>
2019-12-02 22:56:34 +0100

    collate metadata

---
+++ extract_metadata.py
@@ -0,0 +1,26 @@
+import json
+from pathlib import Path
+from constants import DATA_DIR
+
+
+def extract_metadata():
+    # just do everything relative to the readme
+    all_metadata = []
+    for readme_file in Path(DATA_DIR).rglob('*README'):
+        with open(readme_file) as f:
+            metadata = {}
+            for l in f:
+                split = l[:-1].split(':')
+                if len(split) == 2:
+                    metadata[split[0].strip()] = split[1].strip()
+
+        # get the wav file locations
+        metadata['wav_files'] = [str(wav_file) for wav_file in readme_file.parent.parent.rglob('*.wav')]
+        all_metadata.append(metadata)
+            
+    with open('all_metadata.json', 'w') as f:
+        json.dump(all_metadata, f)
+
+
+if __name__ == '__main__':
+    extract_metadata()
